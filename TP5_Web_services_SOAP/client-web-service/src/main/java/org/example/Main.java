27864594b63916aa8cfcbbe1d8cf93feeb7ed9cf
anas-kazay <anaskazay@gmail.com>
2024-05-14 20:48:34 +0100

    add TP6

--- TP5_Web_services_SOAP/client-web-service/src/main/java/org/example/Main.java
+++ TP5_Web_services_SOAP/client-web-service/src/main/java/org/example/Main.java
@@ -9,8 +9,17 @@ public class Main {
         BanqueService proxy = new BanqueWS().getBanqueServicePort();
         System.out.println(proxy.conversionEuroToDH(90));
         Compte compte = proxy.getCompte(4);
-        System.out.println("-------------------
+        System.out.println("-------------------");
         System.out.println(compte.getCode());
         System.out.println(compte.getSolde());
+        System.out.println(compte.getDateCreation());
+
+
+        proxy.listComptes().forEach(c -> {
+            System.out.println("-------------------");
+            System.out.println(c.getCode());
+            System.out.println(c.getSolde());
+            System.out.println(c.getDateCreation());
+        });
     }
 }
